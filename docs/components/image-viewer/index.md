1e5bbe81b622535c91a262a862af5ed57830def5
唐正雄 <tzx@tangzhengxiongdeMacBook-Pro.local>
2022-02-15 22:35:12 +0800

    添加图片加载器

---
+++ docs/components/image-viewer/index.md
@@ -0,0 +1,6 @@
+---
+image-viewer
+---
+
+图片懒加载:
+<code src="../../../packages/image-viewer/demo/base.tsx">
--- src/Foo/Image-viewer/types/action.ts
+++
@@ -1,4 +0,0 @@
-export interface ActionDataType {
-    action: string; // 操作类型
-    data: Record<string, any>; // 操作数据，key/value对象
-}
